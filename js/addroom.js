0e6849c314ff2eadd0f43b38838e9d8f8a230016
Kamyil <plkamil1998@gmail.com>
2018-03-11 00:00:20 +0100

    I'm close to finish 0.9. Just need to fix sum measurment

--- js/addroom.js
+++ js/addroom.js
@@ -1,15 +1,54 @@
-let app = angular.module('my-app', []);
-
-function MyCtrl(){}
-MyCtrl.prototype = {
-  addElement:function(){
-    let newRoom = angular.element('<div class="room"> <h3><input type="text" name="" id="" placeholder="Pole 1"></h3><label for=""><i class="fas fa-arrows-alt-h"></i></label> <input type="number" ng-model="height1"> m <label for=""><i class="fas fa-arrows-alt-v"></i></label> <input type="number" ng-model="width1"> m <br><br><label for="" class="totalSingle">{{ height1*width1 }} m<sup>2</sup></label></div>');
-    let rooms = document.getElementById('rooms');
-    angular.element(rooms).append($compile(newRoom)($scope));
-  }
-};
+let app = angular.module('roomApp',[]);
 
 
 
+app.controller('roomsController', ['$scope', function($scope){
+  let i = 1;
 
-app.controller('myCtrl', MyCtrl);
+$scope.rooms = [
+{
+  name: 'Pole',
+  width: 0,
+  height: 0
+
+},
+{
+  name: 'Pole',
+  width: 0,
+  height: 0
+
+}];
+$scope.total = ($scope.rooms[0].height*$scope.rooms[0].width)+($scope.rooms[1].height*$scope.rooms[1].width);
+
+$scope.addNewTriangleRoom = function() {
+  $scope.rooms.push({
+    name: 'Pole',
+    width: 0,
+    height: 0
+  
+  });
+
+}
+
+$scope.getTotal= function(total){
+  total = $scope.total
+  return total;
+}
+
+$scope.addNewSquareRoom = function() {
+  i++;
+  
+  $scope.rooms.push({
+    name: 'Pole',
+    width: 0,
+    height: 0
+
+  });
+  $scope.total += ($scope.rooms[i].height*$scope.rooms[i].width);
+}
+
+$scope.deleteRoom = function(){
+  $scope.rooms.splice(this.$index, 1);
+}
+
+}])
