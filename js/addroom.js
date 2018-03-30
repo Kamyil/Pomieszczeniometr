cdefa3c89e1ba4ee5bbef7613a43b2485d4ed4c0
Kamyil <plkamil1998@gmail.com>
2018-03-30 12:58:59 +0200

    0.9.2 WORKS PERFECTLY!

--- js/addroom.js
+++ js/addroom.js
@@ -3,21 +3,21 @@ let app = angular.module('roomApp',[]);
 
 
 app.controller('roomsController', ['$scope', function($scope){
- 
+
 
 $scope.rooms = [
 {
   name: 'Pole',
   width: 0,
   height: 0,
-  totalSingle: 0
-
+  totalSingle: 0,
+  
 },{
   name: 'Pole',
   width: 0,
   height: 0,
-  totalSingle: 0
-
+  totalSingle: 0,
+ 
 }];
 $scope.roomsM3 = [
   {
@@ -25,20 +25,21 @@ $scope.roomsM3 = [
     x: 0,
     y: 0,
     z: 0,
-    totalSingle: 0
+    totalSingle: 0,
+  
   
   },{
     name: 'Pole',
     x: 0,
     y: 0,
     z: 0,
-    totalSingle: 0
+    totalSingle: 0,
+  
   
   }];
-  $scope.total= 0;
-  $scope.totalM3 = 0;
 
 
+  
   
   
 // $scope.addNewTriangleRoom = function() {
@@ -53,42 +54,13 @@ $scope.roomsM3 = [
 // }
 
 
-
-function getTotal(){
-  
-  let total=0;
-  angular.forEach($scope.rooms, function(room) {
-    total += room.totalSingle;
-  });
-
-  return total;
-  
-}
-function getTotalM3(){
-  let totalM3=0;
-  angular.forEach($scope.roomsM3, function(roomM3) {
-     totalM3 += roomM3.totalSingle;
-   });
-     console.log(totalM3);
-   return totalM3;
-}
-
-
-
-$scope.$watch(function(newVal,oldVal){
-if(newVal != undefined & newVal != oldVal){
-  $scope.total = getTotal();
-  $scope.totalM3 = getTotalM3();
-}
-  
-});
-
 $scope.totalSingle = function(room){
   room.totalSingle = room.width * room.height;
+  
 
   return room.totalSingle;
+ 
 }
-
 
 
 
@@ -102,10 +74,8 @@ $scope.totalSingleM3 = function(roomM3){
   roomM3.totalSingle = roomM3.x * roomM3.y * roomM3.z;
 
   return roomM3.totalSingle;
+
 }
-
-
-
 
 
 $scope.addNewSquareRoom = function() {
@@ -157,3 +127,45 @@ $scope.deleteRoomM3 = function(){
 
 
 
+app.controller('totalsController', ['$scope', function($scope){
+  $scope.totals = {
+    total: 0,
+    totalM3: 0
+  }
+
+
+
+
+  function getTotal(){
+  
+    let total = 0;
+    angular.forEach($scope.rooms, function(room) {
+      total += room.totalSingle;
+    });
+    console.log(total);
+    return total;
+    
+  }
+  function getTotalM3(){
+ let total = 0;
+    angular.forEach($scope.roomsM3, function(roomM3) {
+      total += roomM3.totalSingle;
+     });
+  
+     return total;
+  }
+$scope.$watch(function(newVal,oldVal){
+if(newVal != oldVal){
+  $scope.totals.total = getTotal();
+  $scope.totals.totalM3 = getTotalM3();
+}
+});
+
+
+
+
+
+
+
+
+}]);
