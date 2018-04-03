070a89baa0bc9460f2a6dce2c3e22240cdd63084
Kamyil <plkamil1998@gmail.com>
2018-04-03 13:01:00 +0200

    0.7.3 - Autoselect enabled

---
+++ js/selectAllOnInput.js
@@ -0,0 +1,16 @@
+// document.querySelector('input[type="number"]').addEventListener('click',function(){
+
+
+//     document.querySelector('input[type="number"]').focus();
+//     console.log('I work!');
+
+
+
+
+
+
+// });
+
+// function selectInput(){
+    
+// }
